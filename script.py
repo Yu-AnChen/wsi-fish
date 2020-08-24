ab414e707b7383b7484682854c5b8704eeb3c26b
Yu-An Chen <atwood12@gmail.com>
2020-08-24 14:03:36 -0400

    Add a second spot detection method - DAOStarFinder from photutils

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ import pandas as pd
 from joblib import Parallel, delayed
 from fish_spot_find_trackpy import (
     locate_with_mask, gmm_filter_spots,
-    trackpy_spots_to_img
+    trackpy_spots_to_img, photutils_daostarfinder
 )
 
 parser = argparse.ArgumentParser()
@@ -43,6 +43,8 @@ print('Files to be precessed')
 print(''.join(['    {}\n'.format(p.name) for p in file_paths]))
 print('File count: {} files\n'.format(len(file_paths)))
 
+method = 'photutils'
+
 channels = args.channels
 save_img = args.save_img
 out_dir = Path(vars(args)['output-dir'])
@@ -73,12 +75,25 @@ for file_path in file_paths[:]:
             'processing channel %d/%d' % (i + 1, len(channels)),
             end='\r'
         )
-        min_peak_int = np.median(np.percentile(img, 95, axis=(1, 2)))
-        spot_results = Parallel(n_jobs=8, verbose=0)(
-            delayed(locate_with_mask)(
-                i, padding_mask=m, min_peak_int=min_peak_int
-            ) for i, m in zip(img, window_mask)
-        )
+
+        if method == 'trackpy':
+            min_peak_int = np.median(np.percentile(img, 95, axis=(1, 2)))
+            spot_results = Parallel(n_jobs=8, verbose=0)(
+                delayed(locate_with_mask)(
+                    i, padding_mask=m, min_peak_int=min_peak_int
+                ) for i, m in zip(img, window_mask)
+            )
+            y, x = 'y', 'x'
+
+        elif method == 'photutils':
+            min_peak_int = np.median(np.percentile(img, 95, axis=(1, 2)))
+            spot_results = Parallel(n_jobs=8, verbose=0)(
+                delayed(photutils_daostarfinder)(
+                    # use two-pass approach to catch out-of-focus spots
+                    i, [min_peak_int*np.exp(1)]*2, [2, 6]
+                ) for i in img
+            )
+            y, x = 'ycentroid', 'xcentroid'
 
         img = rowit.reconstruct_from_windows(
             img.reshape(window_view_shape),
@@ -89,34 +104,54 @@ for file_path in file_paths[:]:
         for idx, df in enumerate(spot_results):
             r, c = np.where(window_position_ref == idx)
             step_size = block_size - overlap_size
-            df.loc[:, 'y'] += (r*step_size - (0.5*overlap_size*(r != 0)))
-            df.loc[:, 'x'] += (c*step_size - (0.5*overlap_size*(c != 0)))
+            df.loc[:, y] += (r*step_size - (0.5*overlap_size*(r != 0)))
+            df.loc[:, x] += (c*step_size - (0.5*overlap_size*(c != 0)))
 
         full_result = pd.concat(spot_results)
-        full_result.drop(columns=['mass', 'ep'], inplace=True)
 
-        mass_dtype = 'int64'
-        if np.issubdtype(img_dtype, np.floating):
-            mass_dtype = 'float64'
-        full_result = full_result.astype(
-            {'raw_mass': mass_dtype, 'signal': str(img_dtype)}
-        )
+        if method == 'trackpy':
+            full_result.drop(columns=['mass', 'ep'], inplace=True)
 
-        filtered = gmm_filter_spots(full_result)
+            mass_dtype = 'int64'
+            if np.issubdtype(img_dtype, np.floating):
+                mass_dtype = 'float64'
+            full_result = full_result.astype(
+                {'raw_mass': mass_dtype, 'signal': str(img_dtype)}
+            )
 
-        csv_name = 'fish_trackpy-{}-channel_{}.csv'.format(img_name, channel)
-        filtered.to_csv(str(out_dir / csv_name))
-        if save_img is True:
+            filtered = gmm_filter_spots(full_result)
+
+            csv_name = 'fish_trackpy-{}-channel_{}.csv'.format(img_name, channel)
+            filtered.to_csv(str(out_dir / csv_name))
+            if save_img is True:
+                append = True
+                if i == 0:
+                    append = False
+                skimage.io.imsave(
+                    str(out_dir / 'fish_trackpy-{}.tif'.format(img_name)),
+                    trackpy_spots_to_img(img_shape, filtered),
+                    bigtiff=True, metadata=None, append=append,
+                    tile=(1024, 1024), photometric='minisblack',
+                    check_contrast=False
+                )
+
+        if method == 'photutils':
+            out_img = np.zeros(img_shape, dtype=np.uint8)
+            out_img[
+                full_result[y].transform(np.round).astype('int'),
+                full_result[x].transform(np.round).astype('int')
+            ] += 1
             append = True
             if i == 0:
                 append = False
             skimage.io.imsave(
-                str(out_dir / 'fish_trackpy-{}.tif'.format(img_name)),
-                trackpy_spots_to_img(img_shape, filtered),
+                str(out_dir / 'fish_photutils-{}.tif'.format(img_name)),
+                out_img,
                 bigtiff=True, metadata=None, append=append,
                 tile=(1024, 1024), photometric='minisblack',
                 check_contrast=False
             )
+
     print()
 
 
