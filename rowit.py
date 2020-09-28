cf87b84443eac3f38c80783ff60b12ff148a93db
Yu-An Chen <atwood12@gmail.com>
2020-09-28 01:33:24 -0400

    Update rowit to class-based implementation

--- rowit.py
+++ rowit.py
@@ -1,59 +1,67 @@
-# pylab
-
 import numpy as np
 from skimage.util import view_as_windows, montage
 
 
-def view_as_windows_overlap(
-    img, block_size, overlap_size, pad_mode='constant',
-    return_padding_mask=False
-): 
-    init_shape = img.shape
-    step_size = block_size - overlap_size
+class WindowView(object):
 
-    padded_shape = np.array(init_shape) + overlap_size
-    n = np.ceil((padded_shape - block_size) / step_size)
-    padded_shape = (block_size + (n * step_size)).astype(np.int)
+    def __init__(
+        self, img_shape, block_size, overlap_size
+    ):
+        self.img_shape = img_shape
+        self.block_size = block_size
+        self.overlap_size = overlap_size
 
-    half = int(overlap_size / 2)
-    img = np.pad(img, (
-        (half, padded_shape[0] - init_shape[0] - half), 
-        (half, padded_shape[1] - init_shape[1] - half),
-    ), mode=pad_mode)
+        self.step_size = block_size - overlap_size
 
-    if return_padding_mask:
-        padding_mask = np.ones(init_shape)
+    def window_view_list(self, img, pad_mode='constant'):
+        half = int(self.overlap_size / 2)
+        img = np.pad(img, (
+            (half, self.padded_shape[0] - self.img_shape[0] - half), 
+            (half, self.padded_shape[1] - self.img_shape[1] - half),
+        ), mode=pad_mode)
+        
+        return self._window_view_list(img)
+    
+    def padding_mask(self):
+        half = int(self.overlap_size / 2)
+        padding_mask = np.ones(self.img_shape, dtype=np.bool)
         padding_mask = np.pad(padding_mask, (
-            (half, padded_shape[0] - init_shape[0] - half), 
-            (half, padded_shape[1] - init_shape[1] - half),
-        ), mode='constant', constant_values=0).astype(np.bool)
+            (half, self.padded_shape[0] - self.img_shape[0] - half), 
+            (half, self.padded_shape[1] - self.img_shape[1] - half),
+        ), mode='constant', constant_values=0)
+        return self._window_view_list(padding_mask)
+
+    def reconstruct(self, img_window_view_list):
+        grid_shape = self.window_view_shape[:2]
+
+        start = int(self.overlap_size / 2)
+        end = int(self.block_size - start)
+
+        img_window_view_list = img_window_view_list[..., start:end, start:end]
+
+        return montage(
+            img_window_view_list, grid_shape=grid_shape
+        )[:self.img_shape[0], :self.img_shape[1]]
+        
+    @property
+    def padded_shape(self):
+        padded_shape = np.array(self.img_shape) + self.overlap_size
+        n = np.ceil((padded_shape - self.block_size) / self.step_size)
+        padded_shape = (self.block_size + (n * self.step_size)).astype(np.int)
+        return tuple(padded_shape)
+
+    @property 
+    def window_view_shape(self):
+        return view_as_windows(
+            np.empty(self.padded_shape), 
+            self.block_size, self.step_size
+        ).shape
+
+    def _window_view_list(self, img):
         return (
-            view_as_windows(img, block_size, step_size),
-            view_as_windows(padding_mask, block_size, step_size)
+            view_as_windows(img, self.block_size, self.step_size)
+                .reshape(-1, self.block_size, self.block_size)
         )
-    else:
-        return view_as_windows(img, block_size, step_size)
-
-def reconstruct_from_windows(
-    window_view, block_size, overlap_size, out_shape=None
-):
-    grid_shape = window_view.shape[:2]
-
-    start = int(overlap_size / 2)
-    end = int(block_size - start)
-
-    window_view = window_view.reshape(
-        (-1, block_size, block_size)
-    )[..., start:end, start:end]
-
-    if out_shape:
-        re, ce = out_shape
-    else:
-        re, ce = None, None
-
-    return montage(
-        window_view, grid_shape=grid_shape, 
-    )[:re, :ce]
 
 def crop_with_padding_mask(img, padding_mask, return_mask=False):
     if np.all(padding_mask == 1):
